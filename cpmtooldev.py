a61d51301b15ebf76ee5cc997b2f45b4d94a0e6f
CPMTooldev <telmnbattr@gmail.com>
2025-04-21 11:28:16 +0800

    Update cpmtooldev.py

--- cpmtooldev.py
+++ cpmtooldev.py
@@ -553,7 +553,7 @@ class CPMTooldev:
     def shittin(self) -> bool: 
         payload = { "account_auth": self.auth_token } 
         params = { "key": self.access_key } 
-        response = requests.post(f"{ENDPOINT_URL}/shittin", params=params, data=payload) 
+        response = requests.post(f"{__ENDPOINT_URL__}/shittin", params=params, data=payload) 
         response_decoded = response.json() 
         return response_decoded.get("ok")
 
